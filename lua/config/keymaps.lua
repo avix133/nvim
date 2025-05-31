fd99f2e38ccd3c983b8c6cf2655a406f3e8b0d25
avix133 <avix133@gmail.com>
2025-05-31 14:37:23 +0200

    remove avix folder

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,55 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>a", "ggVG")
+-- primaegen
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>\\", "<cmd>vsplit<cr>")
+vim.keymap.set("n", "<leader>X", ":.lua<CR>")
+vim.keymap.set("v", "<leader>X", ":lua<CR>")
+vim.keymap.set("n", "<leader>t", "<cmd>")
+
+vim.keymap.set("n", "<leader>t", function()
+  local start_pos = vim.fn.getpos("'<")[2]
+  local end_pos = vim.fn.getpos("'>")[2]
+  for i = start_pos, end_pos do
+    local line = vim.fn.getline(i)
+    print("Line " .. i .. ": " .. line)
+  end
+end, { desc = "Print selected lines" })
+
+function send_reg_to_tmux()
+  local command = vim.fn.getreg('"')
+  local tmux_command = "tmux send-keys -t bottom '" .. command .. "'"
+
+  vim.fn.system(tmux_command)
+end
+
+vim.keymap.set("n", "<leader>t", function()
+  vim.cmd("normal! yy")
+
+  send_reg_to_tmux()
+end, { noremap = true, silent = true })
+
+vim.keymap.set("v", "<leader>t", function()
+  vim.cmd("silent normal! y")
+
+  send_reg_to_tmux()
+end, { noremap = true, silent = true })
